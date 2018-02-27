40bed6e04bc53d0d1c78638dfd45f8c0f8d3b50c
Nicolás Tamargo <reosarevok@gmail.com>
2018-02-27 15:03:45 +0200

    Adding actual send-to-Wikidata code

--- import-tkm-paintings.py
+++ import-tkm-paintings.py
@@ -1,4 +1,4 @@
-import rdflib, requests
+import datetime, rdflib, requests, time
 import pywikibot
 from lxml import etree
 from pywikibot import pagegenerators as pg
@@ -31,7 +31,11 @@ def findtechnique(physical_thing):
         techniqueSection = p2.find('muis:Technique', physical_thing.nsmap)
         technique = techniqueSection.find('crm:P130_shows_features_of', physical_thing.nsmap)
         techniqueResource = technique.xpath('self::*//@rdf:resource', namespaces=physical_thing.nsmap)[0]
-        techniques.append(decodeTechnique(techniqueResource))
+        techniqueWDItem = decodeTechnique(techniqueResource)
+        if techniqueWDItem is not None:
+            techniques.append(techniqueWDItem)
+        else:
+            print "Technique not found: " + techniqueResource
     return techniques
 
 
@@ -40,14 +44,14 @@ def decodeTechnique(technique):
         # 6li
         'http://opendata.muis.ee/thesaurus/107/5329': "Q296955",
         'http://opendata.muis.ee/thesaurus/107/33126': "Q296955",
-        # segatehnika = "mixed technique"
-        'http://opendata.muis.ee/thesaurus/107/5328': "nothing",
+        # segatehnika = "mixed technique" doesn't seem to be a good match for WD "material"
+        'http://opendata.muis.ee/thesaurus/107/5328': None,
         # tempera
         'http://opendata.muis.ee/thesaurus/107/5259': "Q175166",
         # guass
         'http://opendata.muis.ee/thesaurus/107/5539': "Q204330"
     }
-    return switcher.get(technique, "nothing")
+    return switcher.get(technique, None)
 
 def findmaterial(physical_thing):
     materials = []
@@ -56,7 +60,11 @@ def findmaterial(physical_thing):
         materialSection = p45.find('crm:E57_Material', physical_thing.nsmap)
         material = materialSection.find('crm:P130_shows_features_of', physical_thing.nsmap)
         materialResource = material.xpath('self::*//@rdf:resource', namespaces=physical_thing.nsmap)[0]
-        materials.append(decodeMaterial(materialResource))
+        materialWDItem = decodeMaterial(materialResource)
+        if materialWDItem is not None:
+            materials.append(materialWDItem)
+        else:
+            print "Material not found: " + materialResource
     return materials
 
 
@@ -81,7 +89,7 @@ def decodeMaterial(material):
         'http://opendata.muis.ee/thesaurus/112/2410': "Q1397443",
 
     }
-    return switcher.get(material, "nothing")
+    return switcher.get(material, None)
 
 
 def finddimensions(physical_thing):
@@ -114,7 +122,7 @@ def finddimensions(physical_thing):
 def decodeUnit(unit):
     switcher = {
         # cm
-        'http://opendata.muis.ee/thesaurus/200/2526': "cm",
+        'http://opendata.muis.ee/thesaurus/200/2526': "http://www.wikidata.org/entity/Q174728",
     }
     return switcher.get(unit, "nothing")
 
@@ -131,12 +139,11 @@ def findcreationevent(physical_thing):
         # We find the type and extract its URI
         eventTypeXML = eventSection.find('crm:P2_has_type', physical_thing.nsmap)
         eventType = eventTypeXML.xpath('self::*//@rdf:resource', namespaces=physical_thing.nsmap)[0]
-        # Type should be "k2sitski valmistamine" = "making by hand" = 61/11175
-        if eventType == 'http://opendata.muis.ee/thesaurus/61/11175':
+        # Type should be "k2sitski valmistamine" = "making by hand" = 61/11175 or "valmistamine" = making" = 61/11273
+        if (eventType == 'http://opendata.muis.ee/thesaurus/61/11175') or (eventType == 'http://opendata.muis.ee/thesaurus/61/11273'):
             return eventSection
         else:
             print "Not a creation event"
-
 
 
 def findauthors(creation_event):
@@ -148,23 +155,80 @@ def findauthors(creation_event):
         # We extract the type of participant
         participantTypeXML = participantXML.find('crm:P2_has_type', physical_thing.nsmap)
         participantType = participantTypeXML.xpath('self::*//@rdf:resource', namespaces=physical_thing.nsmap)[0]
-        # We check if it's "valmistaja" = "maker" = 58/11400
-        if participantType == 'http://opendata.muis.ee/thesaurus/58/11400':
+        # We check if it's "valmistaja" = "maker" = 58/11400 or "autor" = "author" = 58/11401
+        if (participantType == 'http://opendata.muis.ee/thesaurus/58/11400') or (participantType == 'http://opendata.muis.ee/thesaurus/58/11401'):
             # We find the entry for the author and take its URI
             authorXML = participantXML.find('owl:sameAs', physical_thing.nsmap)
             authorURI = authorXML.xpath('self::*//@rdf:resource', namespaces=physical_thing.nsmap)[0]
-            authors.append(authorURI)
-    return authors
+            authors.append(str(authorURI).rsplit('/', 1)[-1])
+    authorItems = findAuthorItems(authors)
+    return authorItems
+
+
+def findAuthorItems(authorList):
+    itemList = []
+    for author in authorList:
+        query = "SELECT ?item WHERE { ?item wdt:P4889 \"" + author + "\" }"
+        generator = pg.WikidataSPARQLPageGenerator(query, site=site)
+        for item in generator:
+            itemList.append(item.getID())
+    return itemList
+
 
 def findinceptiondate(creation_event):
     dateString = creation_event.find('dcterms:date', physical_thing.nsmap).text
     dateList = dateString.split(' - ')
     inceptiondate = max(dateList)
-    return inceptiondate
+    if validateDate(inceptiondate):
+        return inceptiondate
+    else:
+        return None
+
+
+def validateDate(date_string):
+    try:
+        datetime.datetime.strptime(date_string, '%Y')
+        return True
+    except ValueError:
+        return False
+
+
+def findExistingItems():
+    existingIDs = []
+    query = u'SELECT ?item ?id WHERE { ?item wdt:P195 wd:Q12376420 . ?item wdt:P4525 ?id }'
+    generator = pg.WikidataSPARQLPageGenerator(query, site=site)
+    for item in generator:
+        itemData = item.get()
+        existingIDs.append(itemData['claims'][u'P4525'][0].target)
+    return existingIDs
+
+
+def create_item(site, labels):
+    new_item = pywikibot.ItemPage(site)
+    new_item.editLabels(labels = labels, summary="Setting Estonian label from MuIS")
+    return new_item.getID()
+
+
+def getItemByMuisID(id):
+    query = "SELECT ?item WHERE { ?item wdt:P4525 \"" + id + "\" } LIMIT 1"
+    generator = pg.WikidataSPARQLPageGenerator(query, site=site)
+    for item in generator:
+        return item
+
 
 site = pywikibot.Site("wikidata", "wikidata")
 repo = site.data_repository()
 
+
+# We take the IDs for all TKM paintings already in WD
+existingIDs = findExistingItems()
+
+print existingIDs
+
+# We create a general "stated in" claim for MuIS to reuse every time
+statedin = pywikibot.Claim(repo, "P248")
+muis = pywikibot.ItemPage(repo, "Q50211618")
+statedin.setTarget(muis)
 
 # We take the painting collection from Tartu Art Museum and extract the IDs of all the artworks in it
 collection = rdflib.Graph()
@@ -172,51 +236,157 @@ collection.parse("https://www.muis.ee/rdf/collection/442")
 
 artworkIDs = []
 
+
 for artworkURI in collection.objects(predicate=rdflib.term.URIRef(u'http://www.cidoc-crm.org/cidoc-crm/P46_is_composed_of')):
     artworkIDs.append(str(artworkURI).rsplit('/', 1)[-1])
 
-print artworkIDs
-
-for id in artworkIDs[:1]:
+# If we want to limit the number: for id in artworkIDs[:n]:
+for id in artworkIDs:
     # We take a painting and take all the info we can find
     artworkxml = etree.fromstring(requests.get("https://www.muis.ee/rdf/object/" + id).content)
     physical_thing = artworkxml.find('crm:E18_Physical_Thing', artworkxml.nsmap)
+    wdItem = None
+    # If it's a painting, we want to add data!
     if ispainting(physical_thing):
-        # TODO: create Wikidata item
-        # TODO: change to add Wikidata P4525 property
-        print id
-        # TODO: change to add Wikidata P31 property
-        print "It's a painting"
-        label = findlabel(physical_thing)
-        # TODO: change to add Wikidata et label
-        print label
-        # TODO: change to add Wikidata P217 property
-        identifier = findidentifier(physical_thing)
-        print identifier
+
+        # If the painting is not in WD yet, we create an item
+        if not id in existingIDs:
+            # We extract the provided name for the item and add it as the Estonian label to a new item
+            label = findlabel(physical_thing)
+            print "Creating item with Estonian label " + label
+            new_item_id = create_item(site, {"et": label})
+            # We wait a bit to make sure the new item is available
+            time.sleep(10)
+            # We take the new item
+            wdItem = pywikibot.ItemPage(repo, new_item_id)
+            # We add the MuIS ID to the item
+            muisID = pywikibot.Claim(repo, "P4525")
+            muisID.setTarget(id)
+            wdItem.addClaim(muisID, summary="Importing painting data from the Estonian Museum Portal MuIS")
+            muisID.addSources([statedin],
+                       summary="Importing painting data from the Estonian Museum Portal MuIS")
+            print "Adding MuIS ID " + id
+            # We add it to existingIDs to make sure we don't create it again somehow
+            existingIDs.append(id)
+        else:
+            print "This painting is already in WD - not creating item"
+
+        # The rest we want to do for everything, to add any missing data
+        if wdItem is None:
+            wdItem = getItemByMuisID(id)
+
+        # We extract the claims
+        wdItemData = wdItem.get()
+        wdItemClaims = wdItemData.get("claims")
+
+        # We add the claim the item is an instance of painting
+        if u'P31' not in wdItemClaims:
+            instanceOf = pywikibot.Claim(repo, "P31")
+            paintingQ = pywikibot.ItemPage(repo, "Q3305213")
+            instanceOf.setTarget(paintingQ)
+            wdItem.addClaim(instanceOf, summary="Importing painting data from the Estonian Museum Portal MuIS")
+            instanceOf.addSources([statedin],
+                          summary="Importing painting data from the Estonian Museum Portal MuIS")
+            print "Adding instance of painting"
+
+        # We store the collection WD ID for use in collection and inventory claims
+        TKMQ = pywikibot.ItemPage(repo, "Q12376420")
+
+        # We set the collection to Tartu Art Museum
+        if u'P195' not in wdItemClaims:
+            collectionClaim = pywikibot.Claim(repo, "P195")
+            collectionClaim.setTarget(TKMQ)
+            wdItem.addClaim(collectionClaim, summary="Importing painting data from the Estonian Museum Portal MuIS")
+            collectionClaim.addSources([statedin],
+                              summary="Importing painting data from the Estonian Museum Portal MuIS")
+            print "Adding collection Tartu Art Museum"
+
+        # We find the inventory number and send it to WD
+        if u'P217' not in wdItemClaims:
+            identifier = findidentifier(physical_thing)
+            inventoryNr = pywikibot.Claim(repo, "P217")
+            inventoryNr.setTarget(identifier)
+            wdItem.addClaim(inventoryNr, summary="Importing painting data from the Estonian Museum Portal MuIS")
+            qualifier = pywikibot.Claim(repo, "P195")
+            qualifier.setTarget(TKMQ)
+            inventoryNr.addQualifier(qualifier,
+                           summary="Importing painting data from the Estonian Museum Portal MuIS")
+            inventoryNr.addSources([statedin],
+                           summary="Importing painting data from the Estonian Museum Portal MuIS")
+            print "Adding inventory number " + identifier
         creation_event = findcreationevent(physical_thing)
-        # TODO: change to match and send author data (P170)
-        authors = findauthors(creation_event)
-        for author in authors:
-            print "Author: " + author
-        # TODO: change to send painting date (P571)
-        inception = findinceptiondate(creation_event)
-        print "Date: " + inception
-        # TODO: set collection (P195) Tartu Art Museum (Q12376420)
-        # TODO: change to send technique (P186)
+
+        # We send author data
+        # TODO: check author by author and send any that are missing
+        if u'P170' not in wdItemClaims:
+            authors = findauthors(creation_event)
+            for author in authors:
+                authorClaim = pywikibot.Claim(repo, "P170")
+                authorQ = pywikibot.ItemPage(repo, author)
+                authorClaim.setTarget(authorQ)
+                wdItem.addClaim(authorClaim, summary="Importing painting data from the Estonian Museum Portal MuIS")
+                authorClaim.addSources([statedin],
+                                           summary="Importing painting data from the Estonian Museum Portal MuIS")
+                print "Adding author: " + author
+
+        # We send the inception date
+        if u'P571' not in wdItemClaims:
+            inception = findinceptiondate(creation_event)
+            if inception is not None:
+                wikiInception = pywikibot.WbTime(year=inception)
+                inceptionClaim = pywikibot.Claim(repo, "P571")
+                inceptionClaim.setTarget(wikiInception)
+                wdItem.addClaim(inceptionClaim, summary="Importing painting data from the Estonian Museum Portal MuIS")
+                inceptionClaim.addSources([statedin],
+                                   summary="Importing painting data from the Estonian Museum Portal MuIS")
+                print "Adding inception date: " + inception
+            else:
+                print "Couldn't process the inception date"
+
+        # We extract the info about the "physical thing"
         composedOf = physical_thing.find('crm:P46_is_composed_of', physical_thing.nsmap)
         second_physical_thing = composedOf.find('crm:E18_Physical_Thing', physical_thing.nsmap)
-        techniques = findtechnique(second_physical_thing)
-        for technique in techniques:
-            print "Technique: " + technique
-        # TODO: change to send material (P186)
-        materials = findmaterial(second_physical_thing)
-        for material in materials:
-            print "Material: " + material
-        # TODO: change to send height (P2048)
-        dimensions = finddimensions(second_physical_thing)
-        print "Height: " + dimensions['height'] + " " + dimensions['height-unit']
-        # TODO: change to send width (P2049)
-        print "Width: " + dimensions['width'] + " " + dimensions['width-unit']
 
+        # Material and technique in MuIS are both stored as "material used" (P186) in WD
+        # TODO: check material by material and send any that are missing
+        if u'P186' not in wdItemClaims:
+            techniques = findtechnique(second_physical_thing)
+            materials = findmaterial(second_physical_thing)
+            wdMaterials = list(set().union(techniques, materials))
+            for wdMaterial in wdMaterials:
+                materialClaim = pywikibot.Claim(repo, "P186")
+                wdMaterialQ = pywikibot.ItemPage(repo, wdMaterial)
+                materialClaim.setTarget(wdMaterialQ)
+                wdItem.addClaim(materialClaim, summary="Importing painting data from the Estonian Museum Portal MuIS")
+                materialClaim.addSources([statedin],
+                                           summary="Importing painting data from the Estonian Museum Portal MuIS")
+                print "Adding technique / material: " + wdMaterial
+
+        # We extract the item dimensions
+        dimensions = finddimensions(second_physical_thing)
+
+        # We submit the height. Currently, we do it only if it's a unit in cm
+        if u'P2048' not in wdItemClaims and dimensions['height-unit'] == "http://www.wikidata.org/entity/Q174728":
+            heightClaim = pywikibot.Claim(repo, "P2048")
+            height = pywikibot.WbQuantity(amount=dimensions['height'],
+                                         unit=dimensions['height-unit'],
+                                         site=repo)
+            heightClaim.setTarget(height)
+            wdItem.addClaim(heightClaim, summary="Importing painting data from the Estonian Museum Portal MuIS")
+            heightClaim.addSources([statedin],
+                                     summary="Importing painting data from the Estonian Museum Portal MuIS")
+            print "Adding height: " + dimensions['height'] + " cm"
+
+        # We submit the width. Currently, we do it only if it's a unit in cm
+        if u'P2049' not in wdItemClaims and dimensions['width-unit'] == "http://www.wikidata.org/entity/Q174728":
+            widthClaim = pywikibot.Claim(repo, "P2049")
+            width = pywikibot.WbQuantity(amount=dimensions['width'],
+                                          unit=dimensions['width-unit'],
+                                          site=repo)
+            widthClaim.setTarget(width)
+            wdItem.addClaim(widthClaim, summary="Importing painting data from the Estonian Museum Portal MuIS")
+            widthClaim.addSources([statedin],
+                                   summary="Importing painting data from the Estonian Museum Portal MuIS")
+            print "Adding width: " + dimensions['width'] + " cm"
     else:
-        print "Not a painting"
+        print "Not a painting! ID: " + id
